8c8fa04192e29a806f773ae2f53b150b5965aae3
Juan D. Rodriguez <42884094+Jdrodriguez5@users.noreply.github.com>
2019-02-14 21:08:46 -0800

    Add files via upload

--- BinaryRecursiveSearch.java
+++ BinaryRecursiveSearch.java
@@ -8,16 +8,21 @@ public class BinaryRecursiveSearch implements Practice03Search
 
 	public int search(int[] arr, int target)// , int start, int end
 	{
-		if(arr[0] > arr[arr.length-1])
-			return -1; // base case ... ALWAYS NEED BASE CASE FOR RECURSION
-		int mid = (arr[0] + arr[arr.length-1]) / 2;
+	    int start = arr[0]; // creates the start and end
+	    int end = arr[arr.length-1];
+
+		if(start > end)
+			return -1; // base case
+		int mid = (start + end) / 2;
 		if(arr[mid] == target)
 			return mid;
 		else if (arr[mid] < target)
 		{
+		    arr[0] = mid+1; // makes the start the midpoint plus 1
 			return search(arr, target);//, mid+1, end
-		}           // wasnt sure how to incorporate the start, mid, and end if i only had 2 parameters.
-		else {
+		}           // wasn't sure how to incorporate the start, mid, and end if i only had 2 parameters.
+		else { // i think it would be something like this right?
+		    arr[arr.length-1] = mid-1; // makes the end the midpoint minus 1
 			return search(arr, target);//, start, mid-1
 		}
 	}
